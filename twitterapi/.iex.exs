4c370063c2a275268337e1faee0faeacb420c050
yoshixmk <pirori67@gmail.com>
2015-11-05 23:20:02 +0900

    Could not authenticate you.

---
+++ twitterapi/.iex.exs
@@ -0,0 +1,6 @@
+ExTwitter.configure(
+   consumer_key: System.get_env("ffHLfxCqNQld5CYhjSn7Umjyu"),
+   consumer_secret: System.get_env("s8cdt6P85S9m5JR6YewPy8Dl2awTD3MOcNKW4KbgMiS6MjfWeE"),
+   access_token: System.get_env("913514557-Z2RyFcHz2fXbPxCw6t4G6T4M54G87dKhTPvhuzvA"),
+   access_token_secret: System.get_env("zxfDU3nQbVjSbak69VwP9pMxlJqkcjMWIPSmyB6AYDGVB")
+)
--- twitterapi/lib/twitterapi.ex
+++ twitterapi/lib/twitterapi.ex
@@ -1,2 +1,18 @@
 defmodule Twitter do
+  def search do
+    ExTwitter.search("elixir-lang", [count: 5]) |>
+    Enum.map(fn(tweet) -> tweet.text end) |>
+    Enum.join("\n-----\n") |>
+    IO.puts
+  end
 end
+
+defmodule Example do
+  #コマンドラインから mix run -e "Example.main([])" で実行できる
+  def main([]) do
+    # ここからコードが実行
+    Twitter.search()
+  end
+
+end
+
