55fcdefc7a989caa8ecd9c376412775b50949b78
yoshixmk <pirori67@gmail.com>
2015-11-06 21:25:17 +0900

    Example.main() run ok.

--- twitterapi/lib/twitterapi.ex
+++ twitterapi/lib/twitterapi.ex
@@ -8,9 +8,15 @@ defmodule Twitter do
 end
 
 defmodule Example do
-  #コマンドラインから mix run -e "Example.main([])" で実行できる
-  def main([]) do
-    # ここからコードが実行
+  #コマンドラインから mix run -e "Example.main()" で実行できる
+  def main() do
+    ExTwitter.configure(
+      consumer_key: "ffHLfxCqNQld5CYhjSn7Umjyu",
+      consumer_secret: "s8cdt6P85S9m5JR6YewPy8Dl2awTD3MOcNKW4KbgMiS6MjfWeE",
+      access_token: "913514557-Ov9tvklSG88Kiz6ZQsbzgOPgG5rr0HLmU4mss0u4",
+      access_token_secret: "Y2r6skcG6UQkqNuDKbDcpxyhbfwVYP83xeaIJBUMN30mK"
+    )
+
     Twitter.search()
   end
 
