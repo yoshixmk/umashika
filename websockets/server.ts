78c5985e558a0d527d0726c20fbfd360c5c25d41
yoshixmk <pirori67@gmail.com>
2020-05-29 23:45:11 +0900

    join, leave, clear, chat

--- websockets/server.ts
+++ websockets/server.ts
@@ -31,6 +31,12 @@ export class Transport {
 
 const transport = new Transport();
 
+type User = {
+  username: string
+}
+
+const users = new Map<string, User>();
+
 transport.on("connection", (socket: Socket) => {
   socket.on("test", (data: string) => {
     console.log(socket.id);
@@ -38,9 +44,50 @@ transport.on("connection", (socket: Socket) => {
     socket.emit("RES_TEST", "response");
   });
 
+  // {"route":"join","data": {"username": "ueki"}}
+  socket.on("join", (user: User) => {
+    console.log(socket.id);
+    console.log("join: " + user.username);
+    users.set(socket.id, user);
+    socket.emit("join_done", "ok");
+  });
+
+  // {"route":"leave","data": {"username": "ueki"}}
+  socket.on("leave", (data: string) => {
+    console.log(socket.id);
+    users.delete(socket.id);
+    socket.emit("leave_done", "ok");
+  });
+
+  // {"route":"clear","data": "clear"}}
+  socket.on("clear", (data: string) => {
+    console.log(socket.id);
+    users.clear();
+    socket.emit("clear_done", "ok");
+  });
+
+  type Message = {
+    text: string,
+    openRange?: string
+  }
+  type UserMessage = {
+    user: User,
+    message: Message
+  }
+  // {"route":"chat","data": {"text": "message"}}
+  socket.on("chat", (message: Message) => {
+    console.log(socket.id);
+
+    for (const user of users.values()) {
+      const userMessage = {user, message};
+      socket.emit("chat_broad", userMessage);
+    }
+  });
+
   socket.on("UmaShikaRole_Request", (data: string) => {
     console.log(socket.id);
     console.log(data);
     socket.emit("UmaShikaRole_Response", "uma");
   });
+
 });
