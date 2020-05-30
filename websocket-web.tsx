7820f20a717cfbdc741145b4ba85cd4207a1371e
yoshixmk <pirori67@gmail.com>
2020-05-30 23:44:53 +0900

    App Component Other file

--- websocket-web.tsx
+++ websocket-web.tsx
@@ -1,29 +1,6 @@
-// @deno-types="https://servestjs.org/@/types/react/index.d.ts"
-import React from "https://dev.jspm.io/react/index.js";
-// @deno-types="https://servestjs.org/@/types/react-dom/server/index.d.ts"
-import ReactDOMServer from "https://dev.jspm.io/react-dom/server.js";
-import { createApp } from "https://servestjs.org/@/mod.ts";
-import { send } from "./websockets/client.ts";
+import baseServer from "https://raw.githubusercontent.com/asos-craigmorten/deno-react-base-server/master/mod.tsx";
 
-const app = createApp();
-app.handle("/", async req => {
-  await req.respond({
-    status: 200,
-    headers: new Headers({
-      "content-type": "text/html; charset=UTF-8"
-    }),
-    body: ReactDOMServer.renderToString(
-      <html>
-        <head>
-          <meta charSet="utf-8" />
-          <title>servest</title>
-        </head>
-        <body>
-          <h1>Hello Char Room!</h1>
-          <button onClick={send}>Send!</button>
-        </body>
-      </html>
-    )
-  });
+baseServer({
+  appModulePath: "https://raw.githubusercontent.com/yoshixmk/umashika/feature/deno/websockets/app.tsx",
+  port: 3000,
 });
-app.listen({port: 5000});
