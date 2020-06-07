75e6c335940ba9a4276be5e29b3078c8cda5b9f7
yoshixmk <pirori67@gmail.com>
2020-06-07 12:57:29 +0900

    format

--- semantic-versioning/semantic-versioning.test.ts
+++ semantic-versioning/semantic-versioning.test.ts
@@ -1,7 +1,6 @@
 import {
   assert,
-  assertEquals,
-  assertNotEquals,
+  assertEquals
 } from "https://deno.land/std/testing/asserts.ts";
 import { Semver } from "./semantic-versioning.ts";
 
@@ -11,7 +10,7 @@ Deno.test("gt より大きい時はtrueを返す", () => {
   const semver3 = Semver.ofString("2.2.4");
   assert(semver2.gt(semver1));
   assert(semver3.gt(semver2));
-})
+});
 
 Deno.test("gt より大きくない時はfalseを返す", () => {
   const semver1 = Semver.ofString("1.2.3");
@@ -19,7 +18,7 @@ Deno.test("gt より大きくない時はfalseを返す", () => {
   const semver3 = Semver.ofString("1.2.3");
   assertEquals(semver1.gt(semver2), false);
   assertEquals(semver1.gt(semver3), false);
-})
+});
 
 Deno.test("lt より小さい時はtrueを返す", () => {
   const semver1 = Semver.ofString("1.2.3");
@@ -27,7 +26,7 @@ Deno.test("lt より小さい時はtrueを返す", () => {
   const semver3 = Semver.ofString("2.2.4");
   assert(semver1.lt(semver2));
   assert(semver2.lt(semver3));
-})
+});
 
 Deno.test("lt より小さくない時はfalseを返す", () => {
   const semver1 = Semver.ofString("1.2.3");
@@ -35,7 +34,7 @@ Deno.test("lt より小さくない時はfalseを返す", () => {
   const semver3 = Semver.ofString("1.2.3");
   assertEquals(semver2.lt(semver1), false);
   assertEquals(semver3.lt(semver1), false);
-})
+});
 
 Deno.test("gte 以上の時はtrue そうでない時false", () => {
   const semver1 = Semver.ofString("1.2.3");
@@ -44,7 +43,7 @@ Deno.test("gte 以上の時はtrue そうでない時false", () => {
   assert(semver2.gte(semver1));
   assert(semver3.gte(semver1));
   assertEquals(semver1.gte(semver2), false);
-})
+});
 
 Deno.test("lte 以上の時はtrue そうでない時false", () => {
   const semver1 = Semver.ofString("1.2.3");
@@ -53,7 +52,7 @@ Deno.test("lte 以上の時はtrue そうでない時false", () => {
   assert(semver1.lte(semver2));
   assert(semver1.lte(semver3));
   assertEquals(semver2.lte(semver1), false);
-})
+});
 
 Deno.test("eq 同値の時trueを返す", () => {
   const semver1 = Semver.ofString("1.2.3");
